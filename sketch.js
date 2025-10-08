6036c9cbdee67747a8ec69a8d57b9b776a98f699
mumtaazmb <m.mahir.bare@gmail.com>
2025-10-08 11:21:05 +0100

    Update sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const GRID_SIZE = 20;
+function setup(){
+    createCanvas(600,600)
+
+}
+function draw(){
+    background(0)
+    headX = 40
+    headY = 40
+    rect(headX,headY,20,20)
+    
+
+}
