cbda7256d98475946d77cbb393cb3b5ef369f9a6
mumtaazmb <m.mahir.bare@gmail.com>
2025-10-09 18:06:17 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,27 @@
 const GRID_SIZE = 20;
+let headX = 40;;
+let headY = 40
+let numberOfCells = width / GRID_SIZE;
+let randomCell = random(numberofCells)
+let randomCellFloor = floor(randomCell)
+let foodX;
+let foodY;
+
 function setup(){
-    createCanvas(600,600)
+    createCanvas(600,600);
+    frameRate(3)
+    let foodX = randomCellFloor * GRID_SIZE + GRID_SIZE / 2
+    let foodY = random(600)
 
 }
 function draw(){
-    background(0)
-    headX = 40
-    headY = 40
-    rect(headX,headY,20,20)
+    background(0);
+    rect(headX,headY,20,20);
+    circle(foodX,foodY,GRID_SIZE)
+    headX = headX + GRID_SIZE;
+    if(headX > width){
+        rect(580,headY,20,20)};
+    
     
 
 }
