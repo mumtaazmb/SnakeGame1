0516c5a2883b0ff3da02e9e31940763f9e3ebe91
mumtaazmb <m.mahir.bare@gmail.com>
2025-10-11 19:45:50 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,9 +22,24 @@ function draw(){
     background(0);
     rect(headX,headY,GRID_SIZE,GRID_SIZE);
     circle(foodX + GRID_SIZE / 2,foodY + GRID_SIZE / 2,GRID_SIZE);
-    headX = headX + GRID_SIZE;
+ //   headX = headX + GRID_SIZE;
     if(headX > width){
         rect(580,headY,20,20)};
+    movement()
     
     
+}
+
+function movement(){
+if (keyIsPressed === true) {
+    if (keyCode === UP_ARROW) {
+      headY = headY - 20;
+    } else if (keyCode === DOWN_ARROW) {
+      headY = headY + 20;
+    } else if (keyCode === LEFT_ARROW) {
+      headX = headX - 20;
+    } else if (keyCode === RIGHT_ARROW) {
+      headX = headX + 20;
+    }
+  }
 }
