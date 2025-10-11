20f03ab215a304786901e8271f05e01309ba929f
mumtaazmb <m.mahir.bare@gmail.com>
2025-10-11 18:45:29 +0100

    1 part done

--- sketch.js
+++ sketch.js
@@ -1,27 +1,30 @@
 const GRID_SIZE = 20;
-let headX = 40;;
-let headY = 40
-let numberOfCells = width / GRID_SIZE;
-let randomCell = random(numberofCells)
-let randomCellFloor = floor(randomCell)
+let headX = 40;
+let headY = 40;
 let foodX;
 let foodY;
 
 function setup(){
     createCanvas(600,600);
-    frameRate(3)
-    let foodX = randomCellFloor * GRID_SIZE + GRID_SIZE / 2
-    let foodY = random(600)
+    frameRate(3);
+    let numberofcells = width / GRID_SIZE;
+    let randomcell = random(numberofcells);
+    let randomCellFloor = floor(randomcell);
+    console.log("randomCellFloor:", randomCellFloor);
+
+    foodX = randomCellFloor * GRID_SIZE
+    foodY = floor(random(randomCellFloor)* GRID_SIZE)
 
 }
+
+
 function draw(){
     background(0);
-    rect(headX,headY,20,20);
-    circle(foodX,foodY,GRID_SIZE)
+    rect(headX,headY,GRID_SIZE,GRID_SIZE);
+    circle(foodX + GRID_SIZE / 2,foodY + GRID_SIZE / 2,GRID_SIZE);
     headX = headX + GRID_SIZE;
     if(headX > width){
         rect(580,headY,20,20)};
     
     
-
 }
