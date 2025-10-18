d31d72b6749f0fe752f98fc08ddc18abe25e6f64
mumtaazmb <m.mahir.bare@gmail.com>
2025-10-18 18:43:32 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ let headX = 40;
 let headY = 40;
 let foodX;
 let foodY;
+let xdir = 1
+let ydir = 0
+let gameOver = false
 
 function setup(){
     createCanvas(600,600);
@@ -16,30 +19,51 @@ function setup(){
     foodY = floor(random(randomCellFloor)* GRID_SIZE)
 
 }
-
-
 function draw(){
-    background(0);
+    background(215,181,216);
+    if (!gameOver){
+    fill(255,209,220)
     rect(headX,headY,GRID_SIZE,GRID_SIZE);
+    headX = headX + (GRID_SIZE * xdir)
+    headY = headY + (GRID_SIZE * ydir)
+    fill(25,10,50)
     circle(foodX + GRID_SIZE / 2,foodY + GRID_SIZE / 2,GRID_SIZE);
- //   headX = headX + GRID_SIZE;
-    if(headX > width){
-        rect(580,headY,20,20)};
-    movement()
     
-    
-}
-
-function movement(){
-if (keyIsPressed === true) {
-    if (keyCode === UP_ARROW) {
-      headY = headY - 20;
-    } else if (keyCode === DOWN_ARROW) {
-      headY = headY + 20;
-    } else if (keyCode === LEFT_ARROW) {
-      headX = headX - 20;
-    } else if (keyCode === RIGHT_ARROW) {
-      headX = headX + 20;
+    if (headX >= width || headX < 0 || headY >= height || headY < 0){
+    gameOver = true 
     }
+}
+}
+// function movement(){
+// if (keyIsPressed === true) {
+//     if (keyCode === UP_ARROW) {
+//       xdir = 0
+//       ydir = -1
+//     } else if (keyCode === DOWN_ARROW) {
+//       xdir = 0
+//       ydir = 1
+//     } else if (keyCode === LEFT_ARROW) {
+//       xdir = -1
+//       ydir = 0
+//     } else if (keyCode === RIGHT_ARROW) {
+//       xdir = 1
+//       ydir = 0
+//     }
+//   }
+// }
+
+function keyPressed() {
+  if (keyCode === UP_ARROW ) {
+    xdir = 0;
+    ydir = -1;
+  } else if (keyCode === DOWN_ARROW ) {
+    xdir = 0;
+    ydir = 1;
+  } else if (keyCode === LEFT_ARROW) {
+    xdir = -1;
+    ydir = 0;
+  } else if (keyCode === RIGHT_ARROW ) {
+    xdir = 1;
+    ydir = 0;
   }
 }
